d9f7ebd5b3320ed910f5e04248ecd1bfc5a6ffa7
Jesus <jesus.aranda7@yahoo.com>
2020-02-05 11:02:13 -0600

    front and done wit h CSS, js started.

---
+++ script.js
@@ -0,0 +1,13 @@
+let counter = document.querySelector('.counter');
+const addCount = document.querySelector('#addCountBtn');
+const lowerCount = document.querySelector('#lowerCountBtn');
+
+let count = 0;
+
+addCount.addEventListener('click', incrementCounter);
+
+lowerCount.addEventListener('click', lowerCounter);
+
+function increamentCounter() {
+    
+}
