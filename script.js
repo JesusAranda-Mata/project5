7a3c2c92a94523a3267ac7ad9c8a3fc528a999db
Jesus <jesus.aranda7@yahoo.com>
2020-02-08 10:01:40 -0600

    final commit more modifications in the future

--- script.js
+++ script.js
@@ -2,18 +2,33 @@ let counter = document.querySelector('.counter');
 const addCount = document.querySelector('#addCountBtn');
 const lowerCount = document.querySelector('#lowerCountBtn');
 
-let count = 0;
+let count = 000;
 
 addCount.addEventListener('click', incrementCounter);
 
 lowerCount.addEventListener('click', lowerCounter);
 
 function incrementCounter() {
-    count++;
-    counter.innerHTML = count;
+count++;
+counter.innerHTML = count;
+    if (counter.innerHTML == "000"){
+    counter.style.color = '#d0d0d0';
+    } 
+    else if (counter.innerHTML > "000"){
+    counter.style.color = '#be1d1d';
+    }
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:500,fill:'forwards'});
 }
+
 
 function lowerCounter() {
     count--;
     counter.innerHTML = count;
+    if (counter.innerHTML == "0"){
+        counter.style.color = "#d0d0d0";
+    }
+    else if (counter.innerHTML < "0"){
+        counter.style.color = '#e81616';
+    }
+     counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:500,fill:'forwards'});
 }
